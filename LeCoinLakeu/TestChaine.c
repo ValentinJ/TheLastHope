0e620e3ef868c96c2516f142dde137d8f81551de
Flodov <florian.daudan@gmail.com>
2014-11-27 19:26:30 +0100

    putain ca bug

--- LeCoinLakeu/TestChaine.c
+++ LeCoinLakeu/TestChaine.c
@@ -9,18 +9,50 @@
 #include <time.h>
 #include <sys/msg.h>
 
+/*Gros includes de porc */
+
 #define CLE 123
 
-typedef struct {
-        long  type;
-        pid_t numPID;
-        } tMessage;
+sem_t *sem;
 
 int main(){
 
-	pid_t pid1, pid2;
-	int msg;
-	key_t key;
-	
+	int value;
+	sem = sem_open("/toto",O_RDWR|O_CREAT,066,0);
+	pid_t pid = fork();
+	printf("%i",pid);
+	if(pid == 0){
+		printf("test");
+		sem_getvalue(sem,&value);
+		printf("%d\n",value);
+		printf("Fils entre en attente\n");
+		sem_wait(sem);
+		printf("Fils libéré\n");
+		exit(0);
+	}
+	else{
+	printf("J'attend 1s\n");
+	sleep(1);
+
+	printf("J'affiche la valeur de la semaphore\n");
+
+	sem_getvalue(sem,&value);
+	printf("%d\n",value);
+
+	printf("Je libère le fils\n");
+	sem_post(sem);
+
+	sem_getvalue(sem,&value);
+	printf("%d\n",value);
+
+	wait(NULL);
+	sem_unlink("/toto");
+	sem_destroy(sem);
+	}
+	return 0;
+
+
+
+		
 
 }
