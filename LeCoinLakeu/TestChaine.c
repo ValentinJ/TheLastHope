d04f976db68be78b9f5bdf62b21d3b3328c45544
Flodov <florian.daudan@gmail.com>
2014-11-27 16:03:19 +0100

    Le coin de Lakeu

---
+++ LeCoinLakeu/TestChaine.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <time.h>
+
